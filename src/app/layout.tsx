df47c545e31500546809283983aecaeffc15f7e8
IcedTea <fistfireace37@gmail.com>
2024-05-02 23:44:12 +0800

    updated nav header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMugHot } from "@fortawesome/free-solid-svg-icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="mb-8">
+          <div className="flex justify-between max-w-xl mx-auto px-4 py-4">
+            <Link
+              className="inline-flex gap-1 flex-row-reverse items-center"
+              href={"/"}
+            >
+              <FontAwesomeIcon
+                className="h-8 text-yellow-400"
+                icon={faMugHot}
+              />
+              <span className="mt-2">Buy me a tea</span>
+            </Link>
+            <nav className="mt-2 flex gap-4 items-center">
+              <Link className="" href={"/about"}>
+                About
+              </Link>
+              <Link className="" href={"/faq"}>
+                FAQ
+              </Link>
+              <div className="flex gap-4">
+                <button className="ml-2 border-s-2 border-slate-200 pl-4">
+                  Login
+                </button>
+                <button className="bg-yellow-400 rounded-full px-4 py-2">
+                  Sign Up
+                </button>
+              </div>
+            </nav>
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <>
-      <section className="max-w-lg mx-auto text-center">
+      <section className="max-w-lg mx-auto text-center py-4">
         <h1 className="text-6xl bold">Fund your creativity!</h1>
         <h2 className="mt-4 mb-8">
           Accept support, it&apos;s easier than you think!
         </h2>
-        <button className="bg-yellow-300 px-8 py-4 font-bold rounded">
+        <button className="bg-yellow-300 px-8 py-4 font-bold rounded-full">
           Start my Page
         </button>
       </section>
