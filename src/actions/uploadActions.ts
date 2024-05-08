b30537e847a78e7b23d1adf81c11df35027c2b6d
IcedTea <fistfireace37@gmail.com>
2024-05-08 22:47:44 +0800

    s3 client config, uploadActions functions

--- src/actions/uploadActions.ts
+++ src/actions/uploadActions.ts
@@ -1,8 +1,24 @@
 "use server";
 
-export async function uploadToS3(formData: FormData) {
-  const file = formData.get("file");
-  console.log({ file });
+import { S3Client } from "@aws-sdk/client-s3";
+import uniqid from "uniqid";
 
-  return true;
+export async function uploadToS3(formData: FormData) {
+  const file = formData.get("file") as File;
+
+  const s3Client = new S3Client({
+    region: "ap-southeast-2",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY as string,
+      secretAccessKey: process.env.AWS_SECRET_KEY as string,
+    },
+  });
+
+  const extension = file.name.split(".").slice(-1)[0];
+  const newFileName = uniqid() + "." + extension;
+
+  return {
+    newFileName,
+    extension,
+  };
 }
